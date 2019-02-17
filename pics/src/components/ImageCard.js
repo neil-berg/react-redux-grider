2e25799cf9e4fc58a583abb66590dff37d40cb0f
Neil Berg <neil.berg14@gmail.com>
2019-02-16 20:55:24 -0800

    grid layout

---
+++ pics/src/components/ImageCard.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
+
+  setSpans = e => {
+    console.log(this.imageRef.current.clientHeight);
+  };
+
+  render() {
+    const { description, urls } = this.props.image;
+    return (
+      <div>
+        <img ref={this.imageRef} src={urls.regular} alt={description} />
+      </div>
+    );
+  }
+}
+
+export default ImageCard;
