386750266dd14a974184368515e3574052323d22
Neil Berg <neil.berg14@gmail.com>
2019-02-24 11:43:48 -0800

    redirect from landing page

--- todo/src/components/Landing.js
+++ todo/src/components/Landing.js
@@ -1,12 +1,45 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import './Landing.css';
 
-const Landing = () => {
-  return (
-    <React.Fragment>
-      <h1>Landing Page</h1>
-      <p>Testing the landing page</p>
-    </React.Fragment>
-  );
-};
+class Landing extends React.Component {
+  state = { toMainPage: false };
+
+  onLandingClick = () => {
+    this.setState(() => ({
+      toMainPage: true
+    }));
+  };
+
+  render() {
+    if (this.state.toMainPage) {
+      return <Redirect to="/todo" />;
+    }
+    return (
+      <div className="landing-card ui card">
+        <div className="content">
+          <div className="header">To Do App</div>
+        </div>
+        <div className="content">
+          <h4 className="ui sub header">Simple React ToDo App</h4>
+          <div className="ui small feed">
+            <div className="event">
+              <div className="content">
+                <div className="summary">
+                  Add to-do items and mark them complete
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="extra content">
+          <button className="ui button primary" onClick={this.onLandingClick}>
+            Start new list
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Landing;
