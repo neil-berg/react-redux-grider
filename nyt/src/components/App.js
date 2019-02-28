0afc1dde8072c2c27b62025dc050197c35dfbe67
Neil Berg <neil.berg14@gmail.com>
2019-02-27 22:14:57 -0800

    initial commit

---
+++ nyt/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <p>Hello World</p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ nyt/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './components/Login';
+import App from './components/App';
+import NotFound from './components/NotFound';
+
+ReactDOM.render(
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Login} />
+      <Route exact path="/" component={App} />
+      <Route component={NotFound} />
+    </Switch>
+  </Router>,
+  document.querySelector('#root')
+);
