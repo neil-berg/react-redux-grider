a5bc0ed4f0dd34351484f9d9cd9d7d13c92f0548
Neil Berg <neil.berg14@gmail.com>
2019-03-29 19:49:06 -0700

    non-duplicate user api calls

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,9 +1,23 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+  const uniqueIds = [...new Set(getState().posts.map(post => post.userId))];
+  uniqueIds.forEach(id => dispatch(fetchUser(id)));
+};
+
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get('/posts');
   dispatch({
     type: 'FETCH_POSTS',
+    payload: response.data
+  });
+};
+
+export const fetchUser = id => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+  dispatch({
+    type: 'FETCH_USER',
     payload: response.data
   });
 };
--- blog/src/reducers/postsReducer.js
+++ blog/src/reducers/postsReducer.js
@@ -1,4 +1,4 @@
-const postsReducer = (state = [], action) => {
+export const postsReducer = (state = [], action) => {
   switch (action.type) {
     case 'FETCH_POSTS':
       return action.payload;
@@ -6,5 +6,3 @@ const postsReducer = (state = [], action) => {
       return state;
   }
 };
-
-export default postsReducer;
